182a4d80bf894616fc5da1efd17f11fd6089b184
BahirHakimy <bahirhakimy2020@gmail.com>
2023-08-24 10:46:08 +0430

    allowed hosts fixed

--- autorent/settings_prod.py
+++ autorent/settings_prod.py
@@ -4,7 +4,7 @@ from .settings_base import *
 SECRET_KEY = config("SECRET_KEY")
 STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY")
 JWT_EXPIRATION_TIME_SECONDS = int(config("JWT_EXPIRATION_TIME_SECONDS"))
-ALLOWED_HOSTS = [config("ALLOWED_HOST")]
+ALLOWED_HOSTS = [config("ALLOWED_HOST"), "autorent-rentals.onrender.com"]
 CORS_ALLOWED_ORIGINS = [config("FRONTEND_URL")]
 
 DEBUG = True
